c89739cd247a89f7e5a1426897be9b7c0dd78396
Steffen Winterfeldt <wfeldt@opensuse.org>
2026-03-10 17:21:47 +0100

    greatly speed up internal disk data handling

--- disk.c
+++ disk.c
@@ -23,19 +23,21 @@ disk_t *disk_list;
 
 int disk_read(disk_t *disk, void *buffer, uint64_t block_nr, unsigned count)
 {
-  unsigned factor = disk->block_size / disk->chunk_size;
+  unsigned factor = disk->block_size / DISK_CHUNK_SIZE;
 
   // fprintf(stderr, "read: %llu - %u (factor = %u)\n", (unsigned long long) block_nr, count, factor);
 
-  count *= factor;
-  block_nr *= factor;
+  uint64_t chunk_nr = block_nr * factor;
 
-  for(unsigned u = 0; u < count; u++, block_nr++, buffer += disk->chunk_size) {
-    // fprintf(stderr, "read request: disk %u, addr %08"PRIx64"\n", disk->index, block_nr * disk->chunk_size);
-    if(!disk_cache_read(disk, buffer, block_nr)) {
-      int err = disk_read_single(disk, buffer, block_nr);
+  count *= factor;
+
+  for(unsigned u = 0; u < count; u++, chunk_nr++, buffer += DISK_CHUNK_SIZE) {
+    // fprintf(stderr, "read request: disk %u, addr %08"PRIx64"\n", disk->index, chunk_nr * DISK_CHUNK_SIZE);
+    if(disk_cache_read(disk, &(disk_chunk_t) { .nr = chunk_nr, .data = buffer })) {
+      int err = disk_read_single(disk, buffer, chunk_nr);
       if(err) return err;
-      disk_cache_store(disk, buffer, block_nr);
+      err = disk_cache_store(disk, &(disk_chunk_t) { .nr = chunk_nr, .data = buffer });
+      if(err) return err;
     }
   }
 
@@ -46,15 +48,15 @@ int disk_read(disk_t *disk, void *buffer, uint64_t block_nr, unsigned count)
 int disk_read_single(disk_t *disk, void *buffer, uint64_t block_nr)
 {
   if(disk->fd == -1) {
-    // fprintf(stderr, "cache miss: disk %u, addr %08"PRIx64"\n", disk->index, block_nr * disk->chunk_size);
-    memset(buffer, 0, disk->chunk_size);
+    // fprintf(stderr, "cache miss: disk %u, addr %08"PRIx64"\n", disk->index, block_nr * DISK_CHUNK_SIZE);
+    memset(buffer, 0, DISK_CHUNK_SIZE);
 
     return 0;
   }
 
   // fprintf(stderr, "read: %llu[%llu]\n", (unsigned long long) block_nr, (unsigned long long) disk->size);
 
-  off_t offset = block_nr * disk->chunk_size;
+  off_t offset = block_nr * DISK_CHUNK_SIZE;
 
   if(lseek(disk->fd, offset, SEEK_SET) != offset) {
     fprintf(stderr, "sector %"PRIu64" not found\n", block_nr);
@@ -62,7 +64,7 @@ int disk_read_single(disk_t *disk, void *buffer, uint64_t block_nr)
     return 2;
   }
 
-  if(read(disk->fd, buffer, disk->chunk_size) != disk->chunk_size) {
+  if(read(disk->fd, buffer, DISK_CHUNK_SIZE) != DISK_CHUNK_SIZE) {
     fprintf(stderr, "error reading sector %"PRIu64"\n", block_nr);
 
     return 3;
@@ -72,30 +74,96 @@ int disk_read_single(disk_t *disk, void *buffer, uint64_t block_nr)
 }
 
 
-int disk_cache_read(disk_t *disk, void *buffer, uint64_t chunk_nr)
+int disk_cache_read(disk_t *disk, disk_chunk_t *chunk)
 {
-  for(disk_data_t *disk_data = disk->data; disk_data; disk_data = disk_data->next) {
-    if(disk_data->chunk_nr == chunk_nr) {
-      memcpy(buffer, disk_data->data, disk->chunk_size);
-      return 1;
+  if(!chunk || !chunk->data || chunk->nr == UINT64_MAX) return 1;
+
+  int match;
+  unsigned u = disk_find_chunk(disk, chunk->nr, &match);
+
+  if(!match) return 2;
+
+  memcpy(chunk->data, disk->chunks.list[u].data, DISK_CHUNK_SIZE);
+
+  return 0;
+}
+
+
+int disk_cache_store(disk_t *disk, disk_chunk_t *chunk)
+{
+  if(!chunk || !chunk->data || chunk->nr == UINT64_MAX) return 1;
+
+  int match;
+  unsigned u = disk_find_chunk(disk, chunk->nr, &match);
+
+  if(!match) {
+    if(disk->chunks.len >= DISK_MAX_CHUNKS) return 2;
+    void *buffer = malloc(DISK_CHUNK_SIZE);
+    if(!buffer) return 3;
+    memcpy(buffer, chunk->data, DISK_CHUNK_SIZE);
+    disk->chunks.len++;
+    if(disk->chunks.len > disk->chunks.max) {
+      disk->chunks.max += DISK_CHUNKS_EXTRA;
+      disk->chunks.list = realloc(disk->chunks.list, disk->chunks.max * sizeof (disk_chunk_t));
+      if(disk->chunks.list == NULL) {
+        free(buffer);
+        disk->chunks.len = disk->chunks.max = 0;
+        return 4;
+      }
     }
+    if(u + 1 < disk->chunks.len) {
+      memmove(disk->chunks.list + u + 1, disk->chunks.list + u, sizeof (disk_chunk_t) * (disk->chunks.len - u - 1));
+    }
+    disk->chunks.list[u].nr = chunk->nr;
+    disk->chunks.list[u].data = buffer;
   }
 
   return 0;
 }
 
 
-void disk_cache_store(disk_t *disk, void *buffer, uint64_t chunk_nr)
+// Binary search.
+// If matched, return value is index that matched.
+// If no match, return value is position at which to insert new value (may
+// point at position right after end of the list).
+unsigned disk_find_chunk(disk_t *disk, uint64_t chunk_nr, int *match)
 {
-  // fprintf(stderr, "cache store: disk %u, addr %08"PRIx64"\n", disk->index, chunk_nr * disk->chunk_size);
-  disk_data_t *disk_data = calloc(1, sizeof *disk_data);
+  *match = 0;
 
-  disk_data->chunk_nr = chunk_nr;
-  disk_data->data = malloc(disk->chunk_size);
-  memcpy(disk_data->data, buffer, disk->chunk_size);
+  disk_chunk_t *chunk_list = disk->chunks.list;
 
-  disk_data->next = disk->data;
-  disk->data = disk_data;
+  unsigned u_start = 0;
+  unsigned u_end = disk->chunks.len;
+  unsigned u = 0;
+
+  while(u_end > u_start) {
+    u = (u_end + u_start) / 2;
+
+    int64_t i = chunk_nr - chunk_list[u].nr;
+
+    if(i == 0) {
+      *match = 1;
+      break;
+    }
+
+    if(u_end == u_start + 1) {
+      if(i > 0) u++;
+      break;
+    }
+
+    if(i > 0) {
+      if(u_end == u + 1) {
+        if(i > 0) u++;
+        break;
+      }
+      u_start = u;
+    }
+    else {
+      u_end = u;
+    }
+  }
+
+  return u;
 }
 
 
@@ -113,16 +181,9 @@ int disk_export(disk_t *disk, char *file_name)
 
   fprintf(f, "# disk %u, size = %"PRIu64"\n", disk->index, disk->size_in_bytes);
 
-  uint64_t chunk_nr = 0;
-  disk_data_t *disk_data;
-
-  do {
-    disk_data = disk_cache_search(disk, &chunk_nr);
-    if(disk_data) {
-      disk_cache_dump(disk, disk_data, f);
-    }
+  for(unsigned u = 0; u < disk->chunks.len; u++) {
+    disk_cache_dump(disk, disk->chunks.list + u, f);
   }
-  while(chunk_nr != UINT64_MAX);
 
   if(f != stdout) fclose(f);
 
@@ -132,23 +193,16 @@ int disk_export(disk_t *disk, char *file_name)
 
 int disk_to_fd(disk_t *disk, uint64_t offset)
 {
-  uint64_t chunk_nr = 0;
-  disk_data_t *disk_data;
-
   int fd = syscall(SYS_memfd_create, "", 0);
 
   if(fd == -1) return 0;
 
-  do {
-    disk_data = disk_cache_search(disk, &chunk_nr);
-    if(disk_data) {
-      if(disk_data->chunk_nr * disk->chunk_size >= offset) {
-        lseek(fd, disk_data->chunk_nr * disk->chunk_size - offset, SEEK_SET);
-        write(fd, disk_data->data, disk->chunk_size);
-      }
+  for(unsigned u = 0; u < disk->chunks.len; u++) {
+    if(disk->chunks.list[u].nr * DISK_CHUNK_SIZE >= offset) {
+      lseek(fd, disk->chunks.list[u].nr * DISK_CHUNK_SIZE - offset, SEEK_SET);
+      write(fd, disk->chunks.list[u].data, DISK_CHUNK_SIZE);
     }
   }
-  while(chunk_nr != UINT64_MAX);
 
   lseek(fd, 0, SEEK_SET);
 
@@ -156,19 +210,21 @@ int disk_to_fd(disk_t *disk, uint64_t offset)
 }
 
 
-void disk_cache_dump(disk_t *disk, disk_data_t *disk_data, FILE *file)
+int disk_cache_dump(disk_t *disk, disk_chunk_t *chunk, FILE *file)
 {
+  if(!file) return 1;
+
   uint8_t all_zeros[16] = {};
-  uint8_t *data = disk_data->data;
+  uint8_t *data = chunk->data;
 
   uint64_t max_addr = disk->size_in_bytes - 1;
   unsigned address_digits = 0;
   while(max_addr >>= 4) address_digits++;
   if(address_digits < 4) address_digits = 4;
 
-  for(unsigned u = 0; u < disk->chunk_size; u += 16) {
+  for(unsigned u = 0; u < DISK_CHUNK_SIZE; u += 16) {
     if(memcmp(data + u, &all_zeros, 16)) {
-      fprintf(file, "%0*"PRIx64" ", address_digits, disk_data->chunk_nr * disk->chunk_size + u);
+      fprintf(file, "%0*"PRIx64" ", address_digits, chunk->nr * DISK_CHUNK_SIZE + u);
       for(unsigned u1 = 0; u1 < 16; u1++) {
         fprintf(file, " %02x", data[u + u1]);
       }
@@ -179,28 +235,8 @@ void disk_cache_dump(disk_t *disk, disk_data_t *disk_data, FILE *file)
       fprintf(file, "\n");
     }
   }
-}
 
-
-disk_data_t *disk_cache_search(disk_t *disk, uint64_t *chunk_nr)
-{
-  disk_data_t *disk_data_found = NULL;
-  uint64_t next_chunk_nr = UINT64_MAX;
-
-  if(*chunk_nr == next_chunk_nr) return NULL;
-
-  for(disk_data_t *disk_data = disk->data; disk_data; disk_data = disk_data->next) {
-    if(disk_data->chunk_nr == *chunk_nr) {
-      disk_data_found = disk_data;
-    }
-    if(disk_data->chunk_nr > *chunk_nr && disk_data->chunk_nr < next_chunk_nr) {
-      next_chunk_nr = disk_data->chunk_nr;
-    }
-  }
-
-  *chunk_nr = next_chunk_nr;
-
-  return disk_data_found;
+  return 0;
 }
 
 
@@ -231,7 +267,7 @@ void disk_add_to_list(disk_t *disk)
 void disk_init(char *file_name)
 {
   struct stat sbuf;
-  disk_t disk = { .chunk_size = 512, .block_size = 512 };
+  disk_t disk = { .block_size = DISK_CHUNK_SIZE };
 
   disk.fd = open(file_name, O_RDONLY | O_LARGEFILE);
 
@@ -264,7 +300,7 @@ void disk_import(char *file_name)
 
   disk_t disk = { .fd = -1 };
 
-  uint8_t chunk[512];
+  uint8_t buffer[DISK_CHUNK_SIZE];
   uint64_t current_chunk_nr = UINT64_MAX;
 
   while(getline(&line, &line_len, file) > 0) {
@@ -275,14 +311,14 @@ void disk_import(char *file_name)
     uint8_t line_data[16];
     if(sscanf(line, "# disk %u, size = %"SCNu64"", &index, &size) == 2) {
       if(disk.name) {
-        if(current_chunk_nr != UINT64_MAX) disk_cache_store(&disk, chunk, current_chunk_nr);
+        if(current_chunk_nr != UINT64_MAX) disk_cache_store(&disk, &(disk_chunk_t) { .nr = current_chunk_nr, .data = buffer });
         disk_add_to_list(&disk);
         disk = (disk_t) { .index = disk_list_size };
         current_chunk_nr = UINT64_MAX;
       }
       asprintf(&disk.name, "%s#%u", file_name, index);
       disk.size_in_bytes = size;
-      disk.chunk_size = disk.block_size = 512;
+      disk.block_size = DISK_CHUNK_SIZE;
     }
     else if(
       sscanf(line,
@@ -296,15 +332,13 @@ void disk_import(char *file_name)
       !(addr & 0xf) &&
       addr <= disk.size_in_bytes + 16
     ) {
-      // fprintf(stderr, "XXX %08"PRIx64" %02x %02x\n", addr, line_data[0], line_data[1]);
-      uint64_t chunk_nr = addr / disk.chunk_size;
-      // fprintf(stderr, "ZZZ chunk_nr %"PRIu64", current_chunk_nr %"PRIu64"\n", chunk_nr, current_chunk_nr);
+      uint64_t chunk_nr = addr / DISK_CHUNK_SIZE;
       if(chunk_nr != current_chunk_nr) {
-        if(current_chunk_nr != UINT64_MAX) disk_cache_store(&disk, chunk, current_chunk_nr);
+        if(current_chunk_nr != UINT64_MAX) disk_cache_store(&disk, &(disk_chunk_t) { .nr = current_chunk_nr, .data = buffer });
         current_chunk_nr = chunk_nr;
-        memset(chunk, 0, sizeof chunk);
+        memset(buffer, 0, sizeof buffer);
       }
-      memcpy(chunk + (addr % disk.chunk_size), line_data, 16);
+      memcpy(buffer + (addr % DISK_CHUNK_SIZE), line_data, 16);
     }
     else {
       fprintf(stderr, "%s: line %u: invalid import data: %s\n", file_name, line_nr, line);
@@ -315,7 +349,7 @@ void disk_import(char *file_name)
   free(line);
 
   if(disk.name) {
-    if(current_chunk_nr != UINT64_MAX) disk_cache_store(&disk, chunk, current_chunk_nr);
+    if(current_chunk_nr != UINT64_MAX) disk_cache_store(&disk, &(disk_chunk_t) { .nr = current_chunk_nr, .data = buffer });
     disk_add_to_list(&disk);
   }
 }
--- disk.h
+++ disk.h
@@ -1,10 +1,16 @@
 #include <json-c/json.h>
 
-typedef struct disk_data_s {
-  struct disk_data_s *next;
-  uint64_t chunk_nr;
+// internal block size, fixed
+#define DISK_CHUNK_SIZE		512
+// resize internal chunk list by that amount, if needed
+#define DISK_CHUNKS_EXTRA	256
+// maximum number of chunks to store in internal cache (cache size = 512 MiB)
+#define DISK_MAX_CHUNKS		1024*1024
+
+typedef struct {
+  uint64_t nr;
   uint8_t *data;
-} disk_data_t;
+} disk_chunk_t;
 
 typedef struct {
   char *name;
@@ -14,11 +20,13 @@ typedef struct {
   unsigned sectors;
   unsigned cylinders;
   uint64_t size_in_bytes;
-  unsigned chunk_size;
   unsigned block_size;
   unsigned grub_used:1;
   unsigned isolinux_used:1;
-  disk_data_t *data;
+  struct {
+    disk_chunk_t *list;
+    unsigned len, max;
+  } chunks;
   json_object *json_disk;
   json_object *json_current;
 } disk_t;
@@ -29,10 +37,11 @@ extern disk_t *disk_list;
 int disk_read(disk_t *disk, void *buf, uint64_t sector, unsigned cnt);
 int disk_read_single(disk_t *disk, void *buffer, uint64_t block_nr);
 
-int disk_cache_read(disk_t *disk, void *buffer, uint64_t chunk_nr);
-void disk_cache_dump(disk_t *disk, disk_data_t *disk_data, FILE *file);
-void disk_cache_store(disk_t *disk, void *buffer, uint64_t chunk_nr);
-disk_data_t *disk_cache_search(disk_t *disk, uint64_t *chunk_nr);
+int disk_cache_read(disk_t *disk, disk_chunk_t *chunk);
+int disk_cache_store(disk_t *disk, disk_chunk_t *chunk);
+int disk_cache_dump(disk_t *disk, disk_chunk_t *chunk, FILE *file);
+
+unsigned disk_find_chunk(disk_t *disk, uint64_t chunk_nr, int *match);
 
 int disk_export(disk_t *disk, char *file_name);
 int disk_to_fd(disk_t *disk, uint64_t offset);
